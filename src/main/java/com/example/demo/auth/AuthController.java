96a7c2dcc8281f5d926f353da161d3850812a017
Isabelle Porto <isabelle.porto@idwall.co>
2021-08-22 00:07:40 -0300

    Add Cors configuration

--- src/main/java/com/example/demo/auth/AuthController.java
+++ src/main/java/com/example/demo/auth/AuthController.java
@@ -1,13 +1,13 @@
 package com.example.demo.auth;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    @GetMapping
+    @PostMapping
     public String auth() {
         return "Ok";
     }
